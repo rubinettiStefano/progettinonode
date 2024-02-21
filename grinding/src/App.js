9fd915bf1642f527f53327c53ab91572aa17be8c
Stefano Rubinetti <stefano.rubinetti@gmail.com>
2024-02-21 11:07:55 +0100

    v2

--- grinding/src/App.js
+++ grinding/src/App.js
@@ -1,11 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-      <DadoMalefico />
+      <ContatoreRef />
     </div>
   );
 }
@@ -25,10 +25,62 @@ function DadoMalefico()
   //1) Non REINIZIALIZZO counter ad ogni esecuzione, ma solo una volta, poi mantiene il valore che aveva precedentemente
   //2) Ogni volta che il suo valore CAMBIA la funzione viene richiamata  (il componente viene refreshato, rigraficato)
 
+
+
+  function aggiornaContatore()
+  {
+    let temp = counter;
+    temp++;
+    if(temp==3)
+      temp=100;
+    setCounter(temp);//counter in memoria React vale 3
+    //ma la funzione lo vedrà aggiornato solo dopo essere stata rigraficata
+  }
+
+
   return(
    <>         
         <h1>{counter}</h1>
-        <input type="button" value="aumenta di uno" onClick={()=>setCounter(counter+1)} />
+        <input type="button" value="aumenta di uno" onClick={aggiornaContatore} />
     </>
   );
 }
+
+
+
+
+function ContatoreRef()
+{
+  let counter = useRef(0);//variabile salvata in memoria react, con la differenza rispetto allo state che
+  //il suo cambiamento non refresha la pagina
+
+  
+  function aggiornaContatore()
+  {
+    console.log(counter.current);
+    counter.current++;
+    console.log(counter.current);
+    
+  }
+  
+  let casellina = useRef(null);
+  
+  function stampaCasellina()
+  {
+    casellina.current.style.color="green";
+    //casellina.current -> document.getElementById("id")      .value
+  }
+
+
+  return(
+    <>         
+         <h1>{counter.current}</h1>
+         <input type="button" value="aumenta di uno" onClick={aggiornaContatore} />
+          <br/><br/><br/>
+          
+         <input ref={casellina} style={{color:"red"}} type="text" />
+         <input  type="button" value="stampa" onClick={stampaCasellina} />
+     </>
+   );
+}
+
